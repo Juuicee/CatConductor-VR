0f98f23a4db6155945b1472645810915f17981b3
Kerene Wright <kerene789@gmail.com>
2024-03-06 14:59:21 -0500

    Switching Tracks Movement in Progress

--- Assets/Scripts/Cart_Move.cs
+++ Assets/Scripts/Cart_Move.cs
@@ -16,8 +16,22 @@ public class Cart_Move : MonoBehaviour
     void Update()
     {
         //Moves Forward and back along z axis                           //Up/Down
-    //transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical")* moveSpeed);
+        //transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical")* moveSpeed);
         //Moves Left and right along x Axis                               //Left/Right
-    transform.Translate(Vector3.forward * Time.deltaTime *  moveSpeed);   
+        transform.Translate(Vector3.forward * Time.deltaTime *  moveSpeed);
+
+        // Move Left
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+
+        }
+
+        // Move Right
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
+        //Look into Coroutine for switching
     }
 }
