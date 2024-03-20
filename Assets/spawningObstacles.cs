6b5337c822a506e7121d05e5ed20a7a02c1d3ec4
CrisBlu <55038118+CrisBlu@users.noreply.github.com>
2024-03-20 16:39:42 -0400

    Added chance manipulation and more rock variants

--- Assets/spawningObstacles.cs
+++ Assets/spawningObstacles.cs
@@ -7,9 +7,17 @@ public class spawningObstacles : MonoBehaviour
     // Start is called before the first frame update
 
     public Transform island;
-    public float spawnTimer;
     public List<Transform> spawnLocations = new List<Transform>();
-    public List<GameObject> obstacle = new List<GameObject>();
+    public List<GameObject> smallObstacle = new List<GameObject>();
+    public List<GameObject> mediumObstacle = new List<GameObject>();
+    public List<GameObject> bigObstacle = new List<GameObject>();
+
+    //Chance for each event to happen out of 100
+    // 1 = chance for a single track obstacle to spawn
+    // 2 = chance for a double track obstacle to spawn
+    // 3 = chance for box
+    public List<int> eventsToSpawn = new List<int>();
+    private int totalChance = 0;
     
 
     void Start()
@@ -19,7 +27,13 @@ public class spawningObstacles : MonoBehaviour
             spawnLocations.Add(child.transform);
         }
 
-        StartCoroutine(Spawner());
+        for(int i = 0; i < eventsToSpawn.Count; i++){
+            totalChance += eventsToSpawn[i];
+            eventsToSpawn[i] = totalChance;
+            Debug.Log(eventsToSpawn[i]);
+        }
+
+        StartCoroutine(Spawner(2f));
     }
 
     // Update is called once per frame
@@ -28,25 +42,44 @@ public class spawningObstacles : MonoBehaviour
         
     }
 
-    IEnumerator Spawner(){
+    IEnumerator Spawner(float spawnTimer){
 
         yield return new WaitForSeconds(spawnTimer);
         SpawnEnemy();
-        spawnTimer = Random.Range(1.2f, 2.5f);
-        StartCoroutine(Spawner());
+        
 
     }
 
     void SpawnEnemy(){
-        int trackSpawn = Random.Range(0, 5);
-        //Spawn small rock on one track
-        if(trackSpawn == 0 || trackSpawn == 2 || trackSpawn == 4){
-            Instantiate(obstacle[0], spawnLocations[trackSpawn].position, Quaternion.identity, island); 
-        }
-        else if(trackSpawn == 1 || trackSpawn == 3){
-            Instantiate(obstacle[1], spawnLocations[trackSpawn].position, Quaternion.identity, island);
-        }
-        
 
+        float spawnTimer = 0;
+        int nextAction = Random.Range(0, totalChance);
+
+        if(nextAction < eventsToSpawn[0]){
+
+            int trackSpawn = Random.Range(0, 3);
+            int rock = Random.Range(0, smallObstacle.Count);
+            Instantiate(smallObstacle[rock], spawnLocations[trackSpawn].position, Quaternion.identity, island); 
+
+            spawnTimer = Random.Range(.5f, 1.5f);
+
+        }
+
+        else if(nextAction < eventsToSpawn[1]){
+            int trackSpawn = Random.Range(3, 5);
+            int rock = Random.Range(0, mediumObstacle.Count);
+            Instantiate(mediumObstacle[rock], spawnLocations[trackSpawn].position, Quaternion.identity, island);
+
+            spawnTimer = Random.Range(1.5f, 3f);
+        }
+
+        else if(nextAction < eventsToSpawn[2]){
+            Instantiate(bigObstacle[0], spawnLocations[5].position, Quaternion.identity, island);
+            spawnTimer = Random.Range(2f, 4f);
+        }
+
+
+        
+        StartCoroutine(Spawner(spawnTimer));
     }
 }
