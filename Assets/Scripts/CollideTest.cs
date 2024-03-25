6e383b7ffbe3842ad754a3e0810186da69f19d25
Christian Deangelo Orozco <chrisorozco305@gmail.com>
2024-03-25 14:55:53 -0400

    train sound added

--- Assets/Scripts/CollideTest.cs
+++ Assets/Scripts/CollideTest.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class CollideTest : MonoBehaviour
 {
+    public AudioSource lol;
+    private void Start()
+    {
+        lol.loop = true;
+    }
     void OnCollisionEnter(Collision other)
     {
        
         print(other.gameObject);
         
+    }
+    private void Update()
+    {
+        if (!lol.isPlaying)
+        {
+            lol.Play();
+        }
     }
 }
