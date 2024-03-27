311e7a40decdcfb22f80ad1dd9f30b45965e7cb1
Christian Deangelo Orozco <chrisorozco305@gmail.com>
2024-03-27 16:05:31 -0400

    HP trigger implemented

--- Assets/CartStats.cs
+++ Assets/CartStats.cs
@@ -7,5 +7,28 @@ public class CartStats : MonoBehaviour
     public bool paused = false;
     public int hp;
     public float time;
-    
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.CompareTag("HitBox"))
+        {
+            hp -= 1; //reduce hp by 1
+            Debug.Log("HP: " + hp);
+
+            if (hp <= 0)
+            {
+                Debug.Log("Game Over");
+            }
+        }
+    }
+
+    private void Start()
+    {
+        Debug.Log("HP: " + hp);
+    }
+    private void Update()
+    {
+        
+    }
+
 }
