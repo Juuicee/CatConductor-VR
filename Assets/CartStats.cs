60c855db06c013d15ab0f667581292b7f3d94635
Christian Deangelo Orozco <chrisorozco305@gmail.com>
2024-03-27 16:13:47 -0400

    Update CartStats.cs

--- Assets/CartStats.cs
+++ Assets/CartStats.cs
@@ -31,4 +31,14 @@ public class CartStats : MonoBehaviour
         
     }
 
+    //GameOver scene
+        //undo kinetics feature
+            //make it go flying
+        //use different script
+        //use pause feature
+            //use time.timescale = 1 regular speed, 0 is stop
+
+    //Retry button
+        //reset game
+
 }
